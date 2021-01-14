05d0ba297d6415db058de8e5b362001f91f4636a
Chris Tsai <ctsai0417@gmail.com>
2021-01-14 15:34:05 +0800

    complete the interactive guessing part

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -11,5 +15,38 @@ func main() {
 	// change the initial seed every round
 	rand.Seed(time.Now().UnixNano())
 	secretNumber := rand.Intn(max - min) + min
-	fmt.Println("The secret number is: ", secretNumber)
+
+	fmt.Println("Guess a number between 1 and 100")
+	fmt.Println("Please input your guess")
+
+	attemps := 0
+
+	for {
+		attemps++
+		reader := bufio.NewReader(os.Stdin)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("An error occured while reading input. Please try again", err)
+			return
+		}
+	
+		input = strings.TrimSuffix(input, "\n")
+	
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Invalid input. Please enter an integer value")
+			return
+		}
+	
+		fmt.Println("Your guess is", guess)
+	
+		if guess > secretNumber {
+			fmt.Println("Your guess is bigger than the secret number")
+		} else if guess < secretNumber {
+			fmt.Println("Your guess is smalller than the secret number")
+		} else {
+			fmt.Println("Correct, you Legend! Your guessed right after", attemps, "guesses")
+			break
+		}
+	}
 }
