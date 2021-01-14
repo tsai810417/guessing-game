98c7440b4a2268e5656bfc5f5e55b207c8261043
Chris Tsai <ctsai0417@gmail.com>
2021-01-14 14:41:57 +0800

    generate different secret every round

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 func main() {
-	
+	min, max := 1, 100
+	// change the initial seed every round
+	rand.Seed(time.Now().UnixNano())
+	secretNumber := rand.Intn(max - min) + min
+	fmt.Println("The secret number is: ", secretNumber)
 }
